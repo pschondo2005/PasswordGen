3d8342e760f0a223bc063021546d9033f0f37160
Student: Peter Peter <94848956+pschondo2005@users.noreply.github.com>
2022-01-27 02:07:19 +0000

    almost perfect

--- Main.java
+++ Main.java
@@ -8,29 +8,28 @@ class Main {
       if(t==1)
       {
         //generate Lowercase
-         char[] lower= new char[8] ;
+         char[] lower= new char[64] ;
              int[] lowerInts= new int[1000] ;
-         for(int i=0;i<lowerInts.length;i++)
-        {
-          int k=(int)(97+Math.random()*25);
-          lowerInts[i]=k;
-        }  
-          for(int i=0;i<lower.length;i++)
-        {
-          int st=lowerInts[i];
-          lower[i]=(char)st; 
-         System.out.print(lower[i]);
-
-        }  
+         
         File fileName1=new File("Lower.txt");
         File fileName11=new File("Lowerpw.txt");
         PrintWriter outFile1 = new PrintWriter(fileName1);
         PrintWriter outFile11 = new PrintWriter(fileName11);
-        for(int ch:lowerInts)
+        for(int i=0;i<lowerInts.length;i++)
         {
+          int k=(int)(97+Math.random()*25);
+          lowerInts[i]=k;
+        } 
+        for(int i=0;i<lower.length;i++)
+        {
+          int st=lowerInts[i];
+          lower[i]=(char)st; 
+         outFile11.print(lower[i]);
+         if((i+1)%8==0)
+         outFile11.print("\n");
+        }       
+        for(int ch:lowerInts)
         outFile1.print(ch);   
-        outFile11.print((char)ch);
-        }
         outFile1.close();
         outFile11.close();
 
@@ -40,26 +39,25 @@ class Main {
         //generate Uppercase      
               char[] upper= new char[8] ;
              int[] upperInts= new int[1000] ;
-         for(int i=0;i<upperInts.length;i++)
-        {
-          int k=(int)(65+Math.random()*25);
-          upperInts[i]=k;
-        }  
-          for(int i=0;i<upper.length;i++)
-        {
-          int st=upperInts[i];
-          upper[i]=(char)st; 
-         System.out.print(upper[i]);
-
-        }  
         File fileName2=new File("Upper.txt");
         File fileName22=new File("Upperpw.txt");
         PrintWriter outFile2 = new PrintWriter(fileName2);
         PrintWriter outFile22 = new PrintWriter(fileName22);
+        for(int i=0;i<upperInts.length;i++)
+        {
+          int k=(int)(97+Math.random()*25);
+          upperInts[i]=k;
+        } 
+        for(int i=0;i<upper.length;i++)
+        {
+          int st=upperInts[i];
+          upper[i]=(char)st; 
+         outFile22.print(upper[i]);
+         if((i+1)%8==0)
+         outFile22.print("\n");
+        }       
         for(int ch:upperInts)
         outFile2.print(ch);    
-        for(int ch:upper)
-        outFile22.print(ch); 
         outFile2.close();
         outFile22.close();
 
@@ -69,27 +67,25 @@ class Main {
                 //generate all Case
               int[] mixedLetInts= new int[1000] ;
               char[] mixedLet= new char[8] ;
-          for(int i=0;i<mixedLetInts.length;i++)
-        {
-          int k=(int)(65+Math.random()*50);
-          if(k>90)
-          k+=6;
-          mixedLetInts[i]=k;
-        }  
-          for(int i=0;i<mixedLet.length;i++)
-        {
-          int st=mixedLetInts[i];
-          mixedLet[i]=(char)st;       
-           System.out.print(mixedLet[i]);
-        }  
         File fileName3=new File("Mixed_letters.txt");
         File fileName33=new File("Mixed_letterspw.txt");
         PrintWriter outFile3 = new PrintWriter(fileName3);
         PrintWriter outFile33 = new PrintWriter(fileName33);
+        for(int i=0;i<mixedLetInts.length;i++)
+        {
+          int k=(int)(97+Math.random()*25);
+          mixedLetInts[i]=k;
+        } 
+        for(int i=0;i<mixedLet.length;i++)
+        {
+          int st=mixedLetInts[i];
+          mixedLet[i]=(char)st; 
+         outFile33.print(mixedLet[i]);
+         if((i+1)%8==0)
+         outFile33.print("\n");
+        }       
         for(int ch:mixedLetInts)
-        outFile3.print(ch);   
-        for(int ch:mixedLet)   
-        outFile33.print(ch);      
+        outFile3.print(ch);       
         outFile3.close();
         outFile33.close();
 
@@ -99,29 +95,25 @@ class Main {
                 //generate all Char no Symbols         
                  char[] mixedLetNum= new char[8] ;
                 int[] mixedLetNumInts= new int[1000] ;
-                for(int i=0;i<mixedLetNumInts.length;i++)
-        {
-          int k=(int)(48+Math.random()*60);
-           if(k>57)
-          k+=7;
-           if(k>90)
-          k+=6;
-          mixedLetNumInts[i]=k;
-        }  
-         for(int i=0;i<mixedLetNum.length;i++)
-        {
-          int st=mixedLetNumInts[i];
-          mixedLetNum[i]=(char)st;
-          System.out.print(mixedLetNum[i]);
-        }  
         File fileName4=new File("Mixed_letters_numbers.txt");
-        File fileName44=new File("Mixed_letters_numbers.txt");
+        File fileName44=new File("Mixed_letters_numberspw.txt");
         PrintWriter outFile4 = new PrintWriter(fileName4);
         PrintWriter outFile44 = new PrintWriter(fileName44);
+        for(int i=0;i<mixedLetNumInts.length;i++)
+        {
+          int k=(int)(97+Math.random()*25);
+          mixedLetNumInts[i]=k;
+        } 
+        for(int i=0;i<mixedLetNum.length;i++)
+        {
+          int st=mixedLetNumInts[i];
+          mixedLetNum[i]=(char)st; 
+         outFile44.print(mixedLetNum[i]);
+         if((i+1)%8==0)
+         outFile44.print("\n");
+        }       
         for(int ch:mixedLetNumInts)
         outFile4.print(ch);  
-         for(int ch:mixedLetNum)
-        outFile44.print(ch); 
         outFile4.close();
         outFile44.close();
 
@@ -131,25 +123,25 @@ class Main {
                 //generate All char      
           char[] upperallChars= new char[8] ;
          int[] upperallCharsInts= new int[1000] ;
-                for(int i=0;i<upperallCharsInts.length;i++)
-        {
-          int k=(int)(33+Math.random()*93);
-          upperallCharsInts[i]=k;
-        }  
-         for(int i=0;i<upperallChars.length;i++)
-        {
-          int st=upperallCharsInts[i];
-          upperallChars[i]=(char)st;       
-          System.out.print(upperallChars[i]);
-        }  
         File fileName5=new File("Mixed_chars.txt");
         File fileName55=new File("AllCharspw.txt");
         PrintWriter outFile5 = new PrintWriter(fileName5);
         PrintWriter outFile55 = new PrintWriter(fileName55);
+        for(int i=0;i<upperallCharsInts.length;i++)
+        {
+          int k=(int)(97+Math.random()*25);
+          upperallCharsInts[i]=k;
+        } 
+        for(int i=0;i<upperallChars.length;i++)
+        {
+          int st=upperallCharsInts[i];
+          upperallChars[i]=(char)st; 
+         outFile55.print(upperallChars[i]);
+         if((i+1)%8==0)
+         outFile55.print("\n");
+        }       
         for(int ch:upperallCharsInts)
         outFile5.print(ch);    
-        for(int ch:upperallChars)
-        outFile55.print(ch);    
         outFile5.close();
         outFile55.close();
 
